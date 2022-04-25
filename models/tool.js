ee4a1b31c1796e4d1c1d156a3112bbab6ffb3d5a
MaheshMickey <kumarmaheshn2021@gmail.com>
2022-04-25 12:19:40 -0500

    Modified for validation

--- models/tool.js
+++ models/tool.js
@@ -12,6 +12,4 @@ const toolSchema = mongoose.Schema({
      max:400
  }
 })
- 
-module.exports = mongoose.model("Tool",toolSchema) 
-const valid = mongoose.validate(this.schema);
+module.exports = mongoose.model("Tool",toolSchema)
